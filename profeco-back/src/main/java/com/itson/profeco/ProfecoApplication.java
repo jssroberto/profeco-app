c468664832b29ddcef17bfeb9ba977a025a1ef41
OscarMinjarez <minjarez.dev@gmail.com>
2025-04-28 21:04:42 -0700

    Proyecto backend inicializado

---
+++ profeco-back/src/main/java/com/itson/profeco/ProfecoApplication.java
@@ -0,0 +1,13 @@
+package com.itson.profeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfecoApplication.class, args);
+	}
+
+}
---
+++ profeco-back/src/test/java/com/itson/profeco/ProfecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itson.profeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
