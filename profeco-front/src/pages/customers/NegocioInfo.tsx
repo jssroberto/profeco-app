66cdcee55a237993a74bc0e2c24189e78b1ae14f
Carlos H Sánchez <carloshiramsanchez@gmail.com>
2025-05-15 22:15:53 -0700

    mark store as fav (not working) 404

--- profeco-front/src/pages/customers/NegocioInfo.tsx
+++ profeco-front/src/pages/customers/NegocioInfo.tsx
@@ -20,159 +20,185 @@ const Negociosinfo = () => {
   // token
   const { token } = useAuth();
 
-
   const fetchBusinessData = async () => {
-      try {
-        //  carga de datos del super
-        const { data } = await axios.get(
-          `http://localhost:8080/api/v1/stores/${id}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-
-        let rating = 0;
-        try {
-          const ratingRes = await axios.get(
-            `http://localhost:8080/api/v1/ratings/store/${id}/average-score`
-          );
-
-          if (ratingRes) {
-            rating = ratingRes.data;
-          }
-        } catch {
-          rating = 0;
+    try {
+      //  carga de datos del super
+      const { data } = await axios.get(
+        `http://localhost:8080/api/v1/stores/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
+      );
 
-        setBusinessData({
-          name: data.name,
-          image: data.imageUrl.startsWith("http")
-            ? data.imageUrl
-            : `http://${data.imageUrl}`,
-          rating,
-          totalRatings: Array.isArray(data.ratingsIds)
-            ? data.ratingsIds.length
-            : 0,
-          description: `Ubicación: ${data.location}`,
-        });
-
-        // cargqa de productos SIN OFERTA
-        const { data: storeProducts } = await axios.get(
-          `http://localhost:8080/api/v1/store-products/by-store/${id}/without-offer`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
+      let rating = 0;
+      try {
+        const ratingRes = await axios.get(
+          `http://localhost:8080/api/v1/ratings/store/${id}/average-score`
         );
 
-        const productsWithInfo = await Promise.all(
-          storeProducts.map(async (sp: any) => {
-            try {
-              const { data: product } = await axios.get(
-                `http://localhost:8080/api/v1/products/${sp.productId}`,
-                {
-                  headers: {
-                    Authorization: `Bearer ${token}`,
-                  },
-                }
-              );
-              return {
-                id: sp.productId,
-                name: product.name || "Producto sin nombre",
-                image: product.imageUrl
-                  ? product.imageUrl.startsWith("http")
-                    ? product.imageUrl
-                    : `http://${product.imageUrl}`
-                  : "",
-                price: sp.price,
-              };
-            } catch {
-              return null;
-            }
-          })
-        );
-
-        setProducts(productsWithInfo.filter(Boolean));
-
-        // carga de productos CON OFERTA
-        const { data: offerProducts } = await axios.get(
-          `http://localhost:8080/api/v1/store-product-offers/active-any`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        const filteredOfferProducts = offerProducts.filter(
-          (offer: any) => offer.storeProduct.storeId === id
-        );
-
-        const offersWithInfo = await Promise.all(
-          filteredOfferProducts.map(async (offer: any) => {
-            try {
-              const { data: product } = await axios.get(
-                `http://localhost:8080/api/v1/products/${offer.storeProduct.productId}`,
-                {
-                  headers: {
-                    Authorization: `Bearer ${token}`,
-                  },
-                }
-              );
-              return {
-                id: offer.storeProduct.productId,
-                title: product.name || "Producto sin nombre",
-                description: `Precio original: $${offer.storeProduct.price} - Ahora: $${offer.offerPrice}`,
-                validUntil: offer.offerEndDate,
-                discount:
-                  offer.offerPrice && offer.storeProduct.price
-                    ? Math.round(
-                        100 -
-                          (offer.offerPrice / offer.storeProduct.price) * 100
-                      )
-                    : 0,
-              };
-            } catch {
-              return null;
-            }
-          })
-        );
-
-        setOffers(offersWithInfo.filter(Boolean));
-
-        //console.log("Productos cargados:", productsWithInfo.filter(Boolean)); // para pruebassss
-
-        const { data: ratings } = await axios.get(
-          `http://localhost:8080/api/v1/ratings/store/${id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        // Normaliza el score si es necesario (ajusta según tu backend)
-        const reviewsWithInfo = ratings.map((r: any) => ({
-          id: r.id,
-          score:
-            r.score > 5
-              ? Math.round((r.score / 2147483647) * 5 * 10) / 10
-              : r.score,
-          comment: r.comment,
-          date: r.date,
-          customerName: r.customerName,
-        }));
-        setReviews(reviewsWithInfo);
-
-
-
-      } catch (error) {
-        setBusinessData(null);
-      } finally {
-        setLoading(false);
+        if (ratingRes) {
+          rating = ratingRes.data;
+        }
+      } catch {
+        rating = 0;
       }
-    };
+
+      setBusinessData({
+        name: data.name,
+        image: data.imageUrl.startsWith("http")
+          ? data.imageUrl
+          : `http://${data.imageUrl}`,
+        rating,
+        totalRatings: Array.isArray(data.ratingsIds)
+          ? data.ratingsIds.length
+          : 0,
+        description: `Ubicación: ${data.location}`,
+      });
+
+      // cargqa de productos SIN OFERTA
+      const { data: storeProducts } = await axios.get(
+        `http://localhost:8080/api/v1/store-products/by-store/${id}/without-offer`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const productsWithInfo = await Promise.all(
+        storeProducts.map(async (sp: any) => {
+          try {
+            const { data: product } = await axios.get(
+              `http://localhost:8080/api/v1/products/${sp.productId}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+            return {
+              id: sp.productId,
+              name: product.name || "Producto sin nombre",
+              image: product.imageUrl
+                ? product.imageUrl.startsWith("http")
+                  ? product.imageUrl
+                  : `http://${product.imageUrl}`
+                : "",
+              price: sp.price,
+            };
+          } catch {
+            return null;
+          }
+        })
+      );
+
+      setProducts(productsWithInfo.filter(Boolean));
+
+      // carga de productos CON OFERTA
+      const { data: offerProducts } = await axios.get(
+        `http://localhost:8080/api/v1/store-product-offers/active-any`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const filteredOfferProducts = offerProducts.filter(
+        (offer: any) => offer.storeProduct.storeId === id
+      );
+
+      const offersWithInfo = await Promise.all(
+        filteredOfferProducts.map(async (offer: any) => {
+          try {
+            const { data: product } = await axios.get(
+              `http://localhost:8080/api/v1/products/${offer.storeProduct.productId}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+            return {
+              id: offer.storeProduct.productId,
+              title: product.name || "Producto sin nombre",
+              description: `Precio original: $${offer.storeProduct.price} - Ahora: $${offer.offerPrice}`,
+              validUntil: offer.offerEndDate,
+              discount:
+                offer.offerPrice && offer.storeProduct.price
+                  ? Math.round(
+                      100 - (offer.offerPrice / offer.storeProduct.price) * 100
+                    )
+                  : 0,
+            };
+          } catch {
+            return null;
+          }
+        })
+      );
+
+      setOffers(offersWithInfo.filter(Boolean));
+
+      //console.log("Productos cargados:", productsWithInfo.filter(Boolean)); // para pruebassss
+
+      const { data: ratings } = await axios.get(
+        `http://localhost:8080/api/v1/ratings/store/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const reviewsWithInfo = ratings.map((r: any) => ({
+        id: r.id,
+        score:
+          r.score > 5
+            ? Math.round((r.score / 2147483647) * 5 * 10) / 10
+            : r.score,
+        comment: r.comment,
+        date: r.date,
+        customerName: r.customerName,
+      }));
+      setReviews(reviewsWithInfo);
+
+      const { data: favorites } = await axios.get(
+        "http://localhost:8080/api/v1/preferences/favorite-stores",
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+  
+      setIsFavorite(Array.isArray(favorites) && favorites.some((fav: any) => fav.id === id));
+
+    } catch (error) {
+      setBusinessData(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // metodo para marcar store como fav
+  const handleToggleFavorite = async () => {
+    try {
+      if (isFavorite) {
+        await axios.delete(
+          `http://localhost:8080/api/v1/preferences/favorite-stores/${id}`,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        setIsFavorite(false);
+      } else {
+        await axios.post(
+          `http://localhost:8080/api/v1/preferences/favorite-stores/${id}`,
+          {},
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        setIsFavorite(true);
+      }
+    } catch (e) {
+        console.log(e);
+    }
+  };
 
 
   useEffect(() => {
@@ -188,15 +214,16 @@ const Negociosinfo = () => {
       <BusinessHeader
         {...businessData}
         isFavorite={isFavorite}
-        onToggleFavorite={() => setIsFavorite(!isFavorite)}
+        onToggleFavorite={handleToggleFavorite}
       />
       <OffersSection offers={offers} />
       <ProductsSection products={products} />
       <StoreWish storeId={id || ""} />
-      <ReviewSection 
-        reviews={reviews} 
+      <ReviewSection
+        reviews={reviews}
         storeId={id || ""}
-        onReviewAdded={fetchBusinessData}/>
+        onReviewAdded={fetchBusinessData}
+      />
     </div>
   );
 };
